f07adcd87d433488592a786f9d6c1f2e8b8a04a6
wangxiwei <wangxiwei@haizhi.com>
2016-01-09 10:21:29 +0800

    add popup window

--- WebContent/popup.js
+++ WebContent/popup.js
@@ -1,3 +1,14 @@
 /**
  * Created by haizhi on 16/1/8.
  */
+
+addEventListener('load', function () {
+    document.getElementById("submit").addEventListener('click', function () {
+        var raw_text = document.getElementById("json-text").value;
+        var new_window = window.open("template.html");
+        var pre = new_window.document.createElement("pre");
+        pre.innerText = raw_text;
+
+        new_window.document.body.appendChild(pre);
+    });
+});
